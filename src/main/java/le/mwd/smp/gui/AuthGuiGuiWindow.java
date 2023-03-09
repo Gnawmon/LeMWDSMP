29f42ed4e5349c50f670e6e54f0dc96fe77eb710
liminalityy <122311991+liminalityyy@users.noreply.github.com>
2023-03-09 23:09:14 +0200

    testing

--- src/main/java/le/mwd/smp/gui/AuthGuiGuiWindow.java
+++ src/main/java/le/mwd/smp/gui/AuthGuiGuiWindow.java
@@ -1,15 +1,27 @@
 
 package le.mwd.smp.gui;
 
-import le.mwd.smp.LeMwdSmpMod;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.gui.screen.inventory.ContainerScreen;
+import net.minecraft.client.Minecraft;
+
+import java.util.HashMap;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.matrix.MatrixStack;
 
 @OnlyIn(Dist.CLIENT)
 public class AuthGuiGuiWindow extends ContainerScreen<AuthGuiGui.GuiContainerMod> {
-
 	private World world;
 	private int x, y, z;
 	private PlayerEntity entity;
-
 	private final static HashMap guistate = AuthGuiGui.guistate;
 
 	public AuthGuiGuiWindow(AuthGuiGui.GuiContainerMod container, PlayerInventory inventory, ITextComponent text) {
@@ -28,7 +40,6 @@ public class AuthGuiGuiWindow extends ContainerScreen<AuthGuiGui.GuiContainerMod
 		this.renderBackground(ms);
 		super.render(ms, mouseX, mouseY, partialTicks);
 		this.renderHoveredTooltip(ms, mouseX, mouseY);
-
 	}
 
 	@Override
@@ -38,7 +49,10 @@ public class AuthGuiGuiWindow extends ContainerScreen<AuthGuiGui.GuiContainerMod
 		RenderSystem.defaultBlendFunc();
 
 		Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("le_mwd_smp:textures/screens/bogulied.png"));
-		this.blit(ms, this.guiLeft + 0, this.guiTop + 0, 0, 0, 256, 256, 256, 256);
+		this.blit(ms, this.guiLeft + 0, this.guiTop + -1, 0, 0, 256, 256, 256, 256);
+
+		Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("le_mwd_smp:textures/screens/johnmoe_jang.png"));
+		this.blit(ms, this.guiLeft + 56, this.guiTop + 55, 0, 0, 55, 59, 55, 59);
 
 		RenderSystem.disableBlend();
 	}
@@ -49,7 +63,6 @@ public class AuthGuiGuiWindow extends ContainerScreen<AuthGuiGui.GuiContainerMod
 			this.minecraft.player.closeScreen();
 			return true;
 		}
-
 		return super.keyPressed(key, b, c);
 	}
 
@@ -74,7 +87,5 @@ public class AuthGuiGuiWindow extends ContainerScreen<AuthGuiGui.GuiContainerMod
 	public void init(Minecraft minecraft, int width, int height) {
 		super.init(minecraft, width, height);
 		minecraft.keyboardListener.enableRepeatEvents(true);
-
 	}
-
 }
