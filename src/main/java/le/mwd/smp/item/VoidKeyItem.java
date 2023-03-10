ce9911c0b6b36dfc0bd9a7bbbfebee13b88efc59
Gnawmon <alpa2583@gmail.com>
2023-03-10 19:52:14 +0300

    Added keycard, void key, dismantler dash but it needs to adjusted

---
+++ src/main/java/le/mwd/smp/item/VoidKeyItem.java
@@ -0,0 +1,64 @@
+
+package le.mwd.smp.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import le.mwd.smp.itemgroup.TheBottomOfTheVoidItemGroup;
+import le.mwd.smp.LeMwdSmpModElements;
+
+import java.util.List;
+
+@LeMwdSmpModElements.ModElement.Tag
+public class VoidKeyItem extends LeMwdSmpModElements.ModElement {
+	@ObjectHolder("le_mwd_smp:void_key")
+	public static final Item block = null;
+
+	public VoidKeyItem(LeMwdSmpModElements instance) {
+		super(instance, 16);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(TheBottomOfTheVoidItemGroup.tab).maxStackSize(64).rarity(Rarity.EPIC));
+			setRegistryName("void_key");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Unlock Yourself"));
+		}
+	}
+}
