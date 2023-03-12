9e8ffa04d2bc5afacede2c67646e484d3407d937
Gnawmon <alpa2583@gmail.com>
2023-03-12 20:44:05 +0300

    stuff

--- src/main/java/le/mwd/smp/keybind/DismantlerDashKeyKeyBinding.java
+++ src/main/java/le/mwd/smp/keybind/DismantlerDashKeyKeyBinding.java
@@ -110,9 +110,10 @@ public class DismantlerDashKeyKeyBinding extends LeMwdSmpModElements.ModElement 
 		}
 		if (type == 1) {
 
-			DismantlerDashKeyOnKeyReleasedProcedure
-					.executeProcedure(Stream.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("entity", entity))
-							.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			DismantlerDashKeyOnKeyReleasedProcedure.executeProcedure(Stream
+					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
+							new AbstractMap.SimpleEntry<>("z", z), new AbstractMap.SimpleEntry<>("entity", entity))
+					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 		}
 	}
 }
