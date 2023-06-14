a71a97bdf1170815e177a3427a06f47bcdbb866e
Gnawmon <alpa2583@gmail.com>
2023-06-14 17:39:57 +0300

    alphaver dash is now accurate and fixed the minidash colors

--- src/main/java/le/mwd/smp/procedures/AlphaverDashKeyOnKeyPressedProcedure.java
+++ src/main/java/le/mwd/smp/procedures/AlphaverDashKeyOnKeyPressedProcedure.java
@@ -89,48 +89,7 @@ public class AlphaverDashKeyOnKeyPressedProcedure {
 			}
 			LeMwdSmpModVariables.MapVariables.get(world).AlphaverDashKeyLastPressTime = (world.getWorldInfo().getDayTime());
 			LeMwdSmpModVariables.MapVariables.get(world).syncData(world);
-			entity.setMotion(
-					(Math.sin((yaw / 180) * Math.PI) * (-1) * Math.cos((pitch / 180) * Math.PI)
-							* (Math.abs(entity.getMotion().getX()) + velocity / miniDashDivider)),
-					(Math.sin((pitch / 180) * Math.PI) * (Math.abs(entity.getMotion().getY()) + miniDashDivider / 10) * (-1)),
-					(Math.cos((yaw / 180) * Math.PI) * Math.cos((pitch / 180) * Math.PI)
-							* (Math.abs(entity.getMotion().getZ()) + miniDashDivider / 10)));
-			entity.fallDistance = (float) (0);
-			new Object() {
-				private int ticks = 0;
-				private float waitTicks;
-				private IWorld world;
-
-				public void start(IWorld world, int waitTicks) {
-					this.waitTicks = waitTicks;
-					MinecraftForge.EVENT_BUS.register(this);
-					this.world = world;
-				}
-
-				@SubscribeEvent
-				public void tick(TickEvent.ServerTickEvent event) {
-					if (event.phase == TickEvent.Phase.END) {
-						this.ticks += 1;
-						if (this.ticks >= this.waitTicks)
-							run();
-					}
-				}
-
-				private void run() {
-					if (world instanceof World && !world.isRemote()) {
-						((World) world).playSound(null, new BlockPos(x, y, z),
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
-										.getValue(new ResourceLocation("le_mwd_smp:alphaverdashrecharge")),
-								SoundCategory.PLAYERS, (float) 1, (float) 1);
-					} else {
-						((World) world).playSound(x, y, z,
-								(net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS
-										.getValue(new ResourceLocation("le_mwd_smp:alphaverdashrecharge")),
-								SoundCategory.PLAYERS, (float) 1, (float) 1, false);
-					}
-					MinecraftForge.EVENT_BUS.unregister(this);
-				}
-			}.start(world, (int) 60);
+			entity.setMotion((entity.getMotion().getX() * 5), (entity.getMotion().getY() * 2), (entity.getMotion().getZ() * 5));
 		} else if ((world.getWorldInfo().getDayTime() <= LeMwdSmpModVariables.MapVariables.get(world).AlphaverDashKeyLastPressTime
 				|| world.getWorldInfo().getDayTime() > LeMwdSmpModVariables.MapVariables.get(world).AlphaverDashKeyLastPressTime + 60)
 				&& !(new Object() {
@@ -156,11 +115,7 @@ public class AlphaverDashKeyOnKeyPressedProcedure {
 			}
 			LeMwdSmpModVariables.MapVariables.get(world).AlphaverDashKeyLastPressTime = (world.getWorldInfo().getDayTime());
 			LeMwdSmpModVariables.MapVariables.get(world).syncData(world);
-			entity.setMotion(
-					(Math.sin((yaw / 180) * Math.PI) * (-1) * Math.cos((pitch / 180) * Math.PI) * Math.abs(entity.getMotion().getX()) * velocity),
-					(Math.sin((pitch / 180) * Math.PI) * Math.abs(entity.getMotion().getY()) * velocity * (-1)),
-					(Math.cos((yaw / 180) * Math.PI) * Math.cos((pitch / 180) * Math.PI) * Math.abs(entity.getMotion().getZ()) * velocity));
-			entity.fallDistance = (float) (0);
+			entity.setMotion((entity.getMotion().getX() * 10), (entity.getMotion().getY() * 3), (entity.getMotion().getZ() * 10));
 			new Object() {
 				private int ticks = 0;
 				private float waitTicks;
